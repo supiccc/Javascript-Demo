14db70f10db35140ef8330d76f9e797e72f08d1f
supiccc <supiccc@gmail.com>
2017-07-16 01:25:04 +0800

    1.增加缩进 2.增加demo 显示提醒框

---
+++ alert-displayer/main.js
@@ -0,0 +1,24 @@
+var button = document.querySelector('button');
+
+
+function displayMessage() {
+    var html = document.querySelector('html');
+
+    var panel = document.createElement('div');
+    panel.setAttribute('class', 'msgBox');
+    html.appendChild(panel);
+
+    var msg = document.createElement('p');
+    msg.textContent = "This is a messge box";
+    panel.appendChild(msg);
+
+    var closeBtn = document.createElement('button');
+    closeBtn.textContent = "x";
+    panel.appendChild(closeBtn);
+
+    closeBtn.onclick = function() {
+        panel.parentNode.removeChild(panel);
+    }    
+}
+
+button.onclick = displayMessage;
